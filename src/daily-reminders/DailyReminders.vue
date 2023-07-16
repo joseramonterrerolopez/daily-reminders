d61ef5314100f3d5eee885d22a04522875ffe74a
Jose Ramon Terrero <joseramonterrerolopez@gmail.com>
2023-07-16 10:56:38 +0200

    feat: add header on the app

--- src/daily-reminders/DailyReminders.vue
+++ src/daily-reminders/DailyReminders.vue
@@ -1,12 +1,15 @@
 <template>
   <div>
+    <CurrentDayHeader />
     <RemindersList />
   </div>
 </template>
 <script>
+import CurrentDayHeader from './components/CurrentDayHeader.vue'
 import RemindersList from './components/RemindersList.vue'
 export default {
   components: {
+    CurrentDayHeader,
     RemindersList
   }
 }
